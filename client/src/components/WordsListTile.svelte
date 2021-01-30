24247c078ccfba970f7beacb3de3d9b6380e4197
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-01-30 13:18:55 +0500

    style improvements

--- client/src/components/WordsListTile.svelte
+++ client/src/components/WordsListTile.svelte
@@ -5,31 +5,39 @@
 </script>
 
 <li class="words-list-tile">
-  <h3>{ words.title ?? 'Unknown title' }</h3>
-  <ul>
-    {#each words.items as wordsPair}
+  <h3 class="words-list-tile__title">{ words.title ?? 'Unknown title' }</h3>
+  <h4 class="words-list-tile__divider">***</h4>
+  <ul class="words-list-tile__preview">
+    {#each words.items.slice(0, 2) as wordsPair}
       <li>{wordsPair.word} - {wordsPair.translation}</li>
     {/each}
   </ul>
 </li>
 
 <style>
+  li {
+    list-style-type: none;
+  }
   .words-list-tile {
     display: block;
     background-color: whitesmoke;
-    padding: 1em;
+    padding: 1.5em;
     cursor: pointer;
     box-shadow: 5px 5px 5px 2px rgba(0, 0, 0, .1);
   }
   .words-list-tile:not(:first-of-type) {
     margin-left: 1.2em;
   }
-  ul {
+  .words-list-tile__title {
+    margin: 0;
+  }
+  .words-list-tile__divider {
+    margin: .5em 0;
+  }
+  .words-list-tile__preview {
     display: flex;
     flex-direction: column;
     padding: 0;
-  }
-  li {
-    list-style-type: none;
+    color: lightgray;
   }
 </style>
