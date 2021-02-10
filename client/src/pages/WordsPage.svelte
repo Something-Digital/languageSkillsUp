07388d07101fb536b3a1159a43637a83d0b5084c
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-02-10 21:58:12 +0500

    Added checking

--- client/src/pages/WordsPage.svelte
+++ client/src/pages/WordsPage.svelte
@@ -7,9 +7,22 @@
 
   let words = $wordsList.find((item) => item.id === params.id);
   let direction = Direction.Forward;
+  let inputs: string[] = new Array($wordsList.length + 1).fill('');
+  let results: boolean[] = new Array($wordsList.length + 1).fill(null);
+  const classMap = new Map([
+    [true, 'correct'],
+    [false, 'wrong'],
+    [null, ''],
+  ]);
 
   function changeDirection() {
 		direction = direction === Direction.Forward ? Direction.Backward : Direction.Forward;
+	}
+
+  function checkAnswers() {
+		inputs.forEach((input, i) => {
+      results[i] = words.items[i][direction === Direction.Forward ? 'translation' : 'word'].toLowerCase() === input.trim().toLowerCase();
+    });
 	}
 </script>
 
@@ -17,16 +30,18 @@
 <p>Direction: {direction}</p>
 <button on:click={changeDirection}>Change direction</button>
 <ul class="words__list">
-  {#each words.items as wordsPair}
+  {#each words.items as { word, translation }, i}
     <li class="words__pair">
       {#if direction === Direction.Forward}
-        <span>{wordsPair.word} - </span><input type="text" />
+        <span>{word} - </span>
       {:else}
-        <span>{wordsPair.translation} - </span><input type="text" />
+        <span>{translation} - </span>
       {/if}
+      <input type="text" bind:value={inputs[i]} class={classMap.get(results[i])} />
     </li>
   {/each}
 </ul>
+<button on:click={checkAnswers}>Check answers</button>
 
 <style>
   .words__title {
@@ -38,5 +53,11 @@
   }
   .words__pair {
     list-style-type: none;
+  }
+  .correct {
+    border: 1px solid yellowgreen;
+  }
+  .wrong {
+    border: 1px solid tomato;
   }
 </style>
