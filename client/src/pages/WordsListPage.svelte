2c005fe79ffe2da8823a344104a0e761dea32e3f
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-02-12 17:49:21 +0500

    getting data from server added

--- client/src/pages/WordsListPage.svelte
+++ client/src/pages/WordsListPage.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Button from "../components/Button.svelte";
   import WordsListTile from "../components/WordsListTile.svelte";
   import { wordsList } from "../stores/WordsStore";
+
+  function addNewWordsItem() {
+    wordsList.addWords();
+  }
+
+  onMount(async () => {
+    const response = await fetch(
+      "https://d5dlmshm71fmfl4smcq6.apigw.yandexcloud.net/words/list"
+    );
+    const data = await response.json();
+    data.forEach(item => wordsList.addWords(item));
+  });
 </script>
 
-<Button title="Создать список" fun={() => wordsList.addWords()} />
+<Button title="Создать список" fun={addNewWordsItem} />
 
 <ul>
   {#each $wordsList as words}
--- client/src/stores/WordsStore.ts
+++ client/src/stores/WordsStore.ts
@@ -10,27 +10,7 @@ const wordsTemplate: Words = {
 };
 
 function createWordsList() {
-	const { subscribe, set, update } = writable<Words[]>([
-    {
-      id: 'wordsList1',
-      title: 'Список 1',
-      items: [
-        { id: 'wordsPair1', word: 'Haha', translation: 'Хаха' },
-        { id: 'wordsPair2', word: 'Hoho', translation: 'Хохо' },
-        { id: 'wordsPair3', word: 'Hehe', translation: 'Хехе' },
-      ],
-    },
-    {
-      id: 'wordsList2',
-      title: 'List 3',
-      items: [
-        { id: 'wordsPair4', word: 'Dog', translation: 'Собака' },
-        { id: 'wordsPair5', word: 'Cat', translation: 'Кошка' },
-        { id: 'wordsPair6', word: 'Bird', translation: 'Птица' },
-      ],
-    },
-  ]);
-
+	const { subscribe, set, update } = writable<Words[]>([]);
 	return {
 		subscribe,
 		addWords: (item: Words = wordsTemplate) => update(prev => [ ...prev, item]),
@@ -40,10 +20,6 @@ function createWordsList() {
 }
 
 export const wordsList = createWordsList();
-
-// export const level = writable(
-//   parseInt(localStorage.getItem('level'), 10) || 1,
-// );
 
 // level.subscribe(value => {
 // 	localStorage.setItem('level', value);
