f12d6c292eff7031923bd55eb28a664991e14073
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-03-16 23:12:57 +0500

    wip

--- serverless/functions/words/src/index.js
+++ serverless/functions/words/src/index.js
@@ -3,21 +3,29 @@ const { DocAPIService } = require('yandex-cloud/lib/slydb/docapi/docapi');
 const endpoint = 'https://docapi.serverless.yandexcloud.net/ru-central1/b1gd14lj4q35a4o6dunh/etn00ahdatdd84e89q7s';
 const docApi = new DocAPIService(endpoint);
 
-module.exports.handler = async function (event, context) {
-  // event.payload = { a: b }
+const params = {
+  TableName: 'words',
+  Key:
+  {
+    user_id: 'user1',
+  }
+};
+
+module.exports.handler = async function ({ queryStringParameters }) {
   // queryStringParameters {}, multiValueQueryStringParameters {key: []}, body str, path str
-  const params = {
-    TableName: "words",
-    Key:
-    {
-      "user_id": "user1",
-      "id": "words1"
-    }
-  };
-  const res = await docApi.getItem(params);
-  return {
+  const res = {
     statusCode: 200,
     headers: {"content-type": "application/json"},
-    body: JSON.stringify(event),
   };
+  const { id, list_id } = queryStringParameters;
+  if (id) {
+    params.id = id;
+    const result = await docApi.getItem(params);
+    res.body = JSON.stringify(result);
+  } else if (list_id) {
+
+  } else {
+
+  }
+  return res;
 };
